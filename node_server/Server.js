66ec83720746ec7d2334aae0874eb6f24ab3d93c
jerrytang <tt-tjw@qq.com>
2017-10-27 19:55:03 +0800

    fix node get json cross domain bugs

--- node_server/Server.js
+++ node_server/Server.js
@@ -4,12 +4,16 @@ var cache = require('memory-cache');
 var app = express();
 
 app.put('/api/:id', bodyParser, function (req, res) {
-    cache.put("data",req.body);
+    cache.put("data", req.body);
     res.status(200).send("ok");
 });
 
 app.get('/api/:id', function (req, res) {
     var data = cache.get("data");
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Accept, Content-Type, Origin');
+    res.setHeader('Access-Control-Allow-Credentials', true);
     res.send(data);
 });
 app.listen(3000, function () {
